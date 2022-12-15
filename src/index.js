f700df28ed943c8c97b3b9a603c7765049cad545
RicardoFernandez <rreyesfdz96@gmail.com>
2022-12-14 22:48:42 -0600

    store

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store/index'
 
 ReactDOM.render(
   
+  <Provider store={store}>
   <React.StrictMode>
     <BrowserRouter>
         <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
